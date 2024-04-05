8573ac59184264a3fc0697577e3b4a0d27ea60ac
obynonwane <biostechnologyng@gmail.com>
2024-04-05 13:31:34 +0100

    added xml encoder

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zip_code"`
+	Name    string `json:"full_name" xml:"full_name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zip_code"`
 }
 
 func main() {
@@ -18,6 +19,7 @@ func main() {
 	//define  routes
 	http.HandleFunc("/greet", greet)
 	http.HandleFunc("/customers", getAllCustomers)
+	http.HandleFunc("/customersxml", getAllCustomersXML)
 
 	//starting a server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
@@ -40,4 +42,19 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 
 	//encode the slice of customers into json format (pass IO writer - w)
 	json.NewEncoder(w).Encode(customers)
+}
+
+func getAllCustomersXML(w http.ResponseWriter, r *http.Request) {
+
+	//create a slice of customers
+	customers := []Customer{
+		{Name: "Ahish", City: "New Delhi", Zipcode: "7895759"},
+		{Name: "Obinna", City: "Logand", Zipcode: "78575"},
+	}
+
+	//make the response header to be json (if not it did return as text)
+	w.Header().Add("Content-Type", "application/xml")
+
+	//encode the slice of customers into json format (pass IO writer - w)
+	xml.NewEncoder(w).Encode(customers)
 }
